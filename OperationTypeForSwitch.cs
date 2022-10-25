24e136552c25b687208d0971bbf5f0d42db6bc4b
Trudy-Yasin Erdem <trudyasin@outlook.com>
2022-10-25 09:11:07 +0300

    Add project files.

---
+++ OperationTypeForSwitch.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SwitchCase
+//{
+//    public class OperationTypeForSwitch
+//    {
+//        public int ForSwitch(int typeOfOperation = 0)
+//        {
+//            public enum Operations
+//        {
+//            Sum,
+//            Minus,
+//            Multiply,
+//            Divide
+//        }
+//            return 0;
+//        }
+//}
+//}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+int operationType,valueA=5,valueB=9;
+Console.WriteLine("0,1,2,3 gibi bir değer giriniz");
+operationType = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("İlk sayıyı giriniz");
+valueA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci sayıyı giriniz");
+valueB = Convert.ToInt32(Console.ReadLine());
+Operations selectedOperation = (Operations)operationType;
+
+
+switch (selectedOperation)
+{
+    case Operations.Sum:
+        Console.WriteLine($"{valueA}+{valueB}={valueA + valueB}");
+        break;
+    case Operations.Minus:
+        Console.WriteLine($"{valueA}-{valueB}={valueA - valueB}");
+        break;
+    case Operations.Multiply:
+        Console.WriteLine($"{valueA}*{valueB}={valueA * valueB}");
+        break;
+    case Operations.Divide:
+        Console.WriteLine($"{valueA}/{valueB}={valueA / valueB}");
+        break;
+    default:
+        Console.WriteLine($"\nCould not run any of operations: ");
+        string[] allOperationNames = Operations.GetNames(typeof(Operations));
+        foreach (var item in allOperationNames)
+        {
+            Console.WriteLine(item);
+        }
+        break;
+}
+public enum Operations
+{
+    Sum,
+    Minus,
+    Multiply,
+    Divide,
+}
+
+
+
